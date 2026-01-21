fb87e3f56bd0b8172011abd498322e46d18c87df
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-01-21 11:49:13 -0500

    built main project routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from 'react-router'
 import Home from './pages/home/Home.jsx'
+import Shop from './pages/shop/Shop.jsx'
+import About from './pages/about/About.jsx'
 
 
 const router = createBrowserRouter([
@@ -10,6 +12,19 @@ const router = createBrowserRouter([
     path: "/",
     element: <Home />
   },
+  {
+    path: 'home',
+    element: <Home />
+  },
+  {
+    path: "shop",
+    element: <Shop />
+  },
+  {
+    path: 'about',
+    element: <About/>
+  }
+
   
 ])
 
