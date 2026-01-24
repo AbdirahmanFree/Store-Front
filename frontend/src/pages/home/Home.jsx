dd505d13e0ffbad36999b367c079ec5362b2fc26
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-01-23 19:19:50 -0500

    edit

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
 import Hero from "../../components/hero/Hero"
 import Navbar from "../../components/navbar/Navbar"
 import Carousel from "../../components/carousel/Carousel"
+import Mission from "../../components/mission/Mission"
 import classes from "./Home.module.css"
+
 
 function Home(){
     return (
@@ -13,6 +15,7 @@ function Home(){
             <div className={`${classes.content}`}>
                 <Hero/>
                 <Carousel />
+                <Mission />
             </div>
         </div>
     )
