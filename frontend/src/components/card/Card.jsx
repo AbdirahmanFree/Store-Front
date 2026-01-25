66213987e9f6f1b49ec9aeb5b0032f7e0ed1de33
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-01-25 18:46:47 -0500

    styling add to cart button

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,12 +1,17 @@
+import Cart from "../../pages/cart/Cart"
 import classes from "./card.module.css"
+import CartBtn from "./cart/CartBtn"
 
 function Card({title, price, image}){
     return (
     <div className={`${classes.container}`}>
-        <img className={`${classes.image}`} src={image}/>
-        <span>{title}</span>
-        <h3>${price}</h3>
-        
+        <div className={`${classes.innerContainer}`}>
+            <img className={`${classes.image}`} src={image}/>
+            <span>{title}</span>
+            <h3>${price}</h3>
+            </div>
+        <CartBtn/>
+
     </div>
     )
 }
--- frontend/src/components/card/cart/CartBtn.jsx
+++ frontend/src/components/card/cart/CartBtn.jsx
@@ -1,11 +1,13 @@
 import classes from "./CartBtn.module.css"
-function CartBtn({count}){
+function CartBtn({count, setCount}){
     return (
         <div className={`${classes.container}`}>
-            <div className={`${classes.plus}`}>+</div>
+            <div className={`${classes.minus}`}>−</div>
             <div className={`${classes.display}`}></div>
-            <div className={`${classes.minus}`}>-</div>
+            <div className={`${classes.plus}`}>+</div>
 
         </div>
     )
 }
+
+export default CartBtn
